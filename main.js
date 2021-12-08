07e0b0bd5d74b6d39ef9c8a42e3a45d00152a490
sehwan <goodya1f@gmail.com>
2021-12-08 15:46:29 +0900

    JS: Arrow up

--- main.js
+++ main.js
@@ -14,7 +14,6 @@ document.addEventListener('scroll', () =>{
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
-
    const target = event.target;
    const link = target.dataset.link;
    if(link == null) {
@@ -35,6 +34,21 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+   if(window.scrollY > homeHeight / 2) {
+   arrowUp.classList.add('visible');
+   } else {
+      arrowUp.classList.remove('visible');
+   }
+});
+
+// Handle click on the "arrow up" button
+arrowUp.addEventListener('click', () => {
+   scrollIntoView('#home');
 });
 
 
