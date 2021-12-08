df0cbe618887ebe5b15aa7813a52d0781556751e
sehwan <goodya1f@gmail.com>
2021-12-08 14:50:49 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -21,7 +21,17 @@ navbarMenu.addEventListener('click', (event) => {
    if(link == null) {
       return;
    }
-   console.log(event.target.dataset.link);
-   const scrollTo = document.querySelector(link);
-   scrollTo.scrollIntoView({ behavior: 'smooth'});
+
+   scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContackBtn = document.querySelector('.home__contact');
+homeContackBtn.addEventListener('click', (event) => {
+   scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+   const scrollTo = document.querySelector(selector);
+   scrollTo.scrollIntoView({ behavior: 'smooth'});
+}
