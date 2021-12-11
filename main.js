0633ff3184e05515490dafc5fb7f86cf756519e9
sehwan <goodya1f@gmail.com>
2021-12-11 22:24:26 +0900

    JS: project filtering

--- main.js
+++ main.js
@@ -53,14 +53,27 @@ arrowUp.addEventListener('click', () => {
 
 // Projects
 const workBtnContainer = document.querySelector('.work__categories');
-const projectsContainer =  document.querySelector('.work__projects');
+const projectContainer =  document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 workBtnContainer.addEventListener('click', (event) => {
-   const filter = event.target.dataset.filter;
-   console.log(filter);
+   const filter = event.target.dataset.filter || event.target.parentNode.dataset.filter;
+   if(filter == null) {
+      return;
+   }
+   projectContainer.classList.add('anim-out');
+   setTimeout(() => {
+      projects.forEach((project) => {
+         console.log(project);
+         console.log(project.dataset.type);
+         if ( filter === '*' || filter === project.dataset.type) {
+            project.classList.remove('invisible');
+         } else {
+            project.classList.add('invisible');
+         }
+      });
+      projectContainer.classList.remove('anim-out');
+   }, 300);
 });
-
-
 
 function scrollIntoView(selector) {
    const scrollTo = document.querySelector(selector);
