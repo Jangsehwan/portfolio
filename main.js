11ac132da0cf95f75e55cbcc795c0e63aa88cc1e
sehwan <goodya1f@gmail.com>
2021-12-22 23:28:17 +0900

    JS: Get section,navItems

--- main.js
+++ main.js
@@ -95,3 +95,25 @@ function scrollIntoView(selector) {
    const scrollTo = document.querySelector(selector);
    scrollTo.scrollIntoView({ behavior: 'smooth'});
 }
+
+// 1. 모든 섹션 요소들을 가지고 온다
+// 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
+// 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
+
+const sectionIds = [
+   '#home',
+   '#about',
+   '#skills',
+   '#work', 
+   '#certificates',
+   '#contact',
+];
+const sections = sectionIds.map(id => document.querySelector(id));
+const navItems = sectionIds.map(id => 
+      document.querySelector(`[data-link="${id}"]`)
+   );
+console.log(sections);
+console.log(navItems);
+
+
+
