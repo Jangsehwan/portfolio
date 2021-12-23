4ad493a35b8c3d2dfd1d77dbe6c858dc65a297b4
sehwan <goodya1f@gmail.com>
2021-12-23 22:32:36 +0900

    JS: menu items activation
    add button clicks activation

--- main.js
+++ main.js
@@ -88,13 +88,7 @@ workBtnContainer.addEventListener('click', (event) => {
       });
       projectContainer.classList.remove('anim-out');
    }, 300);
-});
-
-
-function scrollIntoView(selector) {
-   const scrollTo = document.querySelector(selector);
-   scrollTo.scrollIntoView({ behavior: 'smooth'});
-}
+})
 
 // 1. 모든 섹션 요소들과 메뉴아이템들을 가지고 온다
 // 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
@@ -119,11 +113,42 @@ const observerOptions = {
    threshold: 0.3,
 };
 
+let selectedNavIndex = 0;
+let selectedNavItem = navItems[0];
+function selectNavItem(selected) {
+   selectedNavItem.classList.remove('active');
+   selectedNavItem = selected;
+   selectedNavItem.classList.add('active');
+}
+
+function scrollIntoView(selector) {
+   const scrollTo = document.querySelector(selector);
+   scrollTo.scrollIntoView({ behavior: 'smooth'});
+   selectNavItem(navItems[sectionIds.indexOf(selector)]);
+}
 const observerCallback = (entries, observer) => {
    entries.forEach(entry => {
-      console.log(entry.target);
+      if(!entry.isIntersecting && entry.intersectionRatio > 0) {
+         const index = sectionIds.indexOf(`#${entry.target.id}`);
+         // 스크롤링이 아래로 되어서 페이지가 올라옴
+         if(entry.boundingClientRect.y < 0) {
+            selectedNavIndex = index + 1;
+         } else { 
+            selectedNavIndex = index - 1;
+         }
+         
+      }
    });
 };
 
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 sections.forEach(section => observer.observe(section));
+
+window.addEventListener('wheel', () => {
+   if(window.scrollY === 0) {
+      selectedNavIndex = 0;
+   } else if(Math.round(window.scrollY + window.innerHeight) >= document.body.clientHeight) {
+      selectedNavIndex = navItems.length - 1;
+   }
+   selectNavItem(navItems[selectedNavIndex]);
+});
